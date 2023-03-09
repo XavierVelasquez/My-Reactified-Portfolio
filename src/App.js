e9dda17a8719e1edeb438a015a8131d6afc2bd99
XavierVelasquez <xaviervelasquez@gmail.com>
2023-03-09 02:54:43 -0800

    Added forgotten Header component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
+import Header from './components/header';
 import Projects from './components/projects';
-import Nav from './components/nav';
 import About from './components/about';
 import Contact from './components/contact';
 
@@ -10,10 +10,7 @@ import Contact from './components/contact';
 function App() {
   return (
     <div className="App">
-<header>
-  <h1>Xavier V's Portfolio</h1>
-  <Nav />
-</header>
+      <Header />
       <main>
       <section>
       <About />
---
+++ src/components/header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Nav from './nav';
+
+function Header() {
+  return (
+    <header>
+      <h1>Xavier V's Portfolio</h1>
+      <Nav />
+    </header>
+  );
+}
+
+export default Header;
